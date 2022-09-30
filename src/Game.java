8a1512e35cfd5e945c88f1414b5a384b5ddc9684
braindome <antonio.saracino83@gmail.com>
2022-09-30 12:14:15 +0200

    Input error handling added.

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,7 @@ public class Game {
 
     Board board = new Board();
     Random random = new Random();
-    Scanner scan = new Scanner(System.in);
+    //Scanner scan = new Scanner(System.in);
     char turn = ' ';
     char winner = ' ';
 
@@ -16,51 +16,122 @@ public class Game {
     //Plays a turn and hands the next turn to the other player.
     public void turn(Player player1, Player player2) {
         if (turn == 'X') {
-            System.out.println(player2.name + ", make a move.");
+            printMakeAMove(player2);
             play(player1, player2);
             turn = 'O';
         } else if (turn == 'O') {
-            System.out.println(player1.name + ", make a move.");
+            printMakeAMove(player1);
             play(player1, player2);
             turn = 'X';
         }
     }
 
+
+    //Prints whose turn it is.
+    public void printMakeAMove(Player player){
+        System.out.println(player.name + ", make a move.");
+    }
+
+    //Checks if input position is taken.
+    public boolean isInputTaken(int input){
+        System.out.println("isInputTaken: " + (board.positionNumber[input] == 'O' || board.positionNumber[input] == 'X'));
+        return (board.positionNumber[input] == 'O' || board.positionNumber[input] == 'X');
+    }
+
+    //Checks if input range is valid (1 to 9, all the locations on the game board).
+    public boolean isValidRange(int input){
+        return input >= 1 && input <= 9;
+    }
+
     //The selected player makes a move. It then prints the updated board and checks for victory conditions.
     public void play(Player player1, Player player2) {
-        int input = scan.nextInt();
-        if (positionCheck(input)) {
-            if (input > 0 && input <= 9) {
-                if (turn == 'O') {
-                    board.positionNumber[input] = 'X';
-                } else if (turn == 'X') {
-                    board.positionNumber[input] = 'O';
+
+        boolean exitWhileLoop = false;
+        Scanner sc = new Scanner(System.in);
+
+        while (!exitWhileLoop) {
+
+               System.out.println("Entering play method while-loop");
+
+              //int inputPosition = readIntFromUser(scan);
+                String s = sc.nextLine();
+                int inputPosition = Integer.parseInt(s);
+
+
+//            try {
+
+//              if(isValidRange(inputPosition)){
+
+//              positionCheck(inputPosition);
+                boolean isValidInput = isValidRange(inputPosition);
+                System.out.println("isValidRange: " + isValidInput);
+
+                if(isValidInput) {
+                    //Here is a valid input between 1 to 9
+
+                    boolean isTaken = isInputTaken(inputPosition);
+
+                    if (!isTaken) { //kommer tillbaka rätt = false ska vara här
+                        if (turn == 'O') {
+                            System.out.println("turn O - placing X");
+                            board.positionNumber[inputPosition] = 'X';
+                        } else if (turn == 'X') {
+                            System.out.println("turn X - placing O");
+                            board.positionNumber[inputPosition] = 'O';
+                        }
+                        exitWhileLoop = true;
+                    } else {
+                        System.out.println("Position is taken; please select another location.");
+                    }
+                } else {
+                    System.out.println("Error. Out of boundaries.");
                 }
-            } else {
-                System.out.println("Input not valid. Please insert an integer from 1 to 9.");           //Fix input error-handling
-            }
+
+//
+//            } catch (Exception e) {
+//
+////                inputPosition = readIntFromUser(scan);
+////                exitWhileLoop = true;
+//            }
         }
 
         board.printBoard();
         winCondition(board, player1, player2);
     }
 
-    //Checks if position picked is occupied.
-    public boolean positionCheck(int n) {
-        if (board.positionNumber[n] == 'O' || board.positionNumber[n] == 'X') {
-            System.out.println("Position is taken; please select another location.");                   //Fix position overlap error-handling
-            return false;
-        }
-        return true;
-    }
+    //Checks if input is valid (The input must be 1) an integer 2) between and included 1 and 9). UPDATE: Incorporated into play().
+//    public int readIntFromUser(Scanner scan) {
+//        while (true) {
+//            try {
+//                String s = scan.nextLine();
+//                return Integer.parseInt(s);
+//            } catch (Exception e) {
+//                System.out.println("Try again");
+//            }
+//        }
+//    }
+
+
+    //Checks if position picked is valid (assuming input is a valid integer) UPDATE: no longer in used. See: isInputTaken.
+//    public int positionCheck(int n) {
+//
+//        while (true) {
+//            // While true take input from scanner
+//            if (board.positionNumber[n] == 'O' || board.positionNumber[n] == 'X') {
+//                System.out.println("Position is taken; please select another location.");       //Fix position overlap error-handling
+//                n = readIntFromUser(scan);
+//
+//            } else break;
+//        }
+//        return n;
+//
+//    }
 
     //Selects a random player to begin the game.
     public void randomPlayerStart(Player[] players) {
         int plNum = random.nextInt(2);
         turn = players[plNum].symbol;
     }
-
-
 
 
 
